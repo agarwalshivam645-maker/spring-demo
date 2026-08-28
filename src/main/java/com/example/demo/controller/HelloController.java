22b6faec9593ff4239df788a0ad0ada4081c2de0
shivam agarwal <agarwal.shivam645@gmail.com>
2026-08-28 17:15:10 +0530

    Version testing

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	
 	@GetMapping("/")
 	public String hello() {
-		return "Hello from Spring Boot - GitHub Actions";
+		return "Hello from Spring Boot - GitHub Actions + version testing";
 	}
 
 }
